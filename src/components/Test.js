b38d8194225ae5129c070b93912bbae838727c48
crono628 <84817352+crono628@users.noreply.github.com>
2022-05-24 20:07:43 -0400

    can find multiple characters and identify a winner

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,43 +1,44 @@
-import { ClickAwayListener } from '@mui/material';
+import { ClickAwayListener, Paper, Stack } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useState, useRef } from 'react';
+import { locations } from './locations';
 
 const Test = () => {
   const [popup, setPopup] = useState(false);
-  const [coord, setCoord] = useState([0, 0]);
+  const [popupCoord, setPopupCoord] = useState([0, 0]);
+  const [choiceCoord, setChoiceCoord] = useState([0, 0]);
   const imageRef = useRef();
 
-  const winner = (e) => {
+  const imgChoiceCoord = (e) => {
     const { pageX, pageY } = e;
+    let theX = Number(imageRef.current.offsetWidth / pageX).toFixed(2);
+    let theY = Number(imageRef.current.offsetHeight / pageY).toFixed(2);
+    setChoiceCoord([theX, theY]);
+    console.log(theX, theY);
+  };
 
-    let theX = Number.parseFloat(imageRef.current.offsetWidth / pageX).toFixed(
-      2
-    );
-    let theY = Number.parseFloat(imageRef.current.offsetWidth / pageY).toFixed(
-      2
-    );
-    let winArr = [
-      { topLeft: { x: 31.3, y: 2.35 }, bottomRight: { x: 17.56, y: 2.16 } },
-    ];
+  const checkForWin = (selection) => {
+    let choice = locations.find((item) => item.name === selection);
 
-    for (const key in winArr) {
-      const element = winArr[key];
+    let validX =
+      choiceCoord[0] <= choice.topLeft.x &&
+      choiceCoord[0] >= choice.bottomRight.x;
+    let validY =
+      choiceCoord[1] <= choice.topLeft.y &&
+      choiceCoord[1] >= choice.bottomRight.y;
 
-      let validX = theX <= element.topLeft.x && theX >= element.bottomRight.x;
-      let validY = theY <= element.topLeft.y && theY >= element.bottomRight.y;
-
-      if (validX && validY) {
-        console.log('winner!');
-      } else {
-        console.log('try again');
-      }
+    if (validX && validY) {
+      console.log('winner!');
+    } else {
+      console.log('try again');
     }
-    console.log(popup);
+
+    setPopup(!popup);
   };
 
   const imgClick = (e) => {
     setPopup(!popup);
-    setCoord([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
+    setPopupCoord([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
   };
 
   return (
@@ -48,19 +49,40 @@ const Test = () => {
             src="https://3hwuuuxcz5o651g144s0kw10-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/G3M_Wheres_Waldo.jpg"
             className="waldo1"
             ref={imageRef}
-            onClick={imgClick}
+            onClick={(e) => {
+              imgClick(e);
+              imgChoiceCoord(e);
+            }}
           />
 
           {popup ? (
             <Box
               sx={{
                 position: 'absolute',
-                left: coord[0],
-                top: coord[1],
-                backgroundColor: 'white',
+                left: popupCoord[0],
+                top: popupCoord[1],
+                backgroundColor: 'lightgray',
+                border: '3px solid black',
+                borderRadius: '10px',
               }}
             >
-              Click me, I will stay visible until you click outside.
+              <Stack spacing={2}>
+                {locations.map((character, index) => {
+                  return (
+                    <Paper
+                      onClick={() => checkForWin(character.name)}
+                      key={index}
+                      style={{
+                        margin: '10px',
+                        padding: '10px',
+                        cursor: 'pointer',
+                      }}
+                    >
+                      {character.name}
+                    </Paper>
+                  );
+                })}
+              </Stack>
             </Box>
           ) : null}
         </div>
---
+++ src/components/locations.js
@@ -0,0 +1,14 @@
+let locations = [
+  {
+    name: 'Odlaw',
+    topLeft: { x: 31.3, y: 1.32 },
+    bottomRight: { x: 17.14, y: 1.22 },
+  },
+  {
+    name: 'Waldo',
+    topLeft: { x: 1.26, y: 31.15 },
+    bottomRight: { x: 1.23, y: 13.06 },
+  },
+];
+
+export { locations };
